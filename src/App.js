f9d375304f3b7701650ff94a33e0587c7750ccef
gitbush-test <82594147+megi-j@users.noreply.github.com>
2023-02-23 11:51:51 +0400

    style: add header

--- src/App.js
+++ src/App.js
@@ -1,25 +1,70 @@
-import logo from './logo.svg';
-import './App.css';
+import styled, { createGlobalStyle } from "styled-components";
+import moon from "./images/moon.png";
+import { Helmet } from "react-helmet";
 
+const GlobalStyles = createGlobalStyle`
+  *{
+      margin: 0;
+      padding: 0;
+      box-sizing: border-box;
+      list-style-type: none;
+      text-decoration: none;
+  }
+  html{
+    font-family: "Nunito Sans";
+  }
+`;
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+      <GlobalStyles />
+      <Helmet>
+        <link
+          href="https://fonts.googleapis.com/css2?family=Nunito+Sans:wght@300;400;600;800&display=swap"
+          rel="stylesheet"
+        />
+      </Helmet>
+      <Header>
+        <Title>Where in the world?</Title>
+        <ModeBox>
+          <img src={moon} alt="" />
+          <ModeText>Dark Mode</ModeText>
+        </ModeBox>
+      </Header>
+    </Container>
   );
 }
 
 export default App;
+
+const Container = styled.div`
+  max-width: 1440px;
+  width: 100%;
+  margin: 0 auto;
+`;
+const Header = styled.header`
+  width: 100%;
+  height: 80px;
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+  background: #ffffff;
+  box-shadow: 0px 2px 4px rgba(0, 0, 0, 0.0562443);
+`;
+const Title = styled.h2`
+  font-weight: 800;
+  font-size: 24px;
+  color: #111517;
+`;
+const ModeBox = styled.div`
+  width: 108px;
+  height: 22px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`;
+const ModeText = styled.p`
+  font-weight: 600;
+  font-size: 16px;
+  color: #111517;
+`;
